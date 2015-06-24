0f9a1134d9d10a8d0cf7712e58a9df05019bec69
Jon Skulski <jskulski@gmail.com>
2015-06-24 00:46:10 -0700

    Using jest as testing harness

---
+++ __tests__/acceptance-test.js
@@ -0,0 +1,12 @@
+describe('Test harness', function() {
+	it('should work', function() {
+		expect(true).toBe(true);
+	})
+});
+
+
+describe('Card', function() {
+	it('should display the correct text in the card', function(){
+		var Card = require('../app/scripts/Card.js');
+	});
+});
