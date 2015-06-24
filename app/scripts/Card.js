d649a16e4d4bc67427bff108cccdb9c9e8a81163
Jon Skulski <jskulski@gmail.com>
2015-06-24 16:52:30 -0700

    Mocha test for react components, not sure if I even need it. But framework. Testing with travis-ci now.

--- app/scripts/Card.js
+++ app/scripts/Card.js
@@ -1,2 +1,13 @@
-var Card = {};
+var React = require('react');
+
+var Card = React.createClass({
+  render: function() {
+    return (
+      <label>
+        <textarea />
+      </label>
+    );
+  }
+});
+
 module.exports = Card;
--- test/react_component_acceptance_spec.js
+++ test/react_component_acceptance_spec.js
@@ -1,7 +1,34 @@
-var should = require('chai').should() // nb. call func
+var should = require('chai').should(); // nb. call func
 
 describe('test harness', function () {
   it('should work correctly', function () {
-    true.should.equal(true)
+    true.should.equal(true);
+  });
+});
+
+
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+
+var Card = require('../app/scripts/Card');
+
+describe('Card Component', function () {
+
+  it('can be rendered into the docuemnt', function () {
+    var renderedComponent = TestUtils.renderIntoDocument(
+      <Card />
+    );
+
+    var inputComponent = TestUtils.findRenderedDOMComponentWithTag(
+      renderedComponent,
+      'TEXTAREA'
+    );
+
+    this.inputElement = inputComponent.getDOMNode();
+  });
+
+  it('should have a textarea', function () {
+    this.inputElement.tagName.should.equal('TEXTAREA');
   })
-})
+
+});
