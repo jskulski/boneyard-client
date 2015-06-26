f94bba944e04c22bb14d7b530a25d1a9d881750e
Jon Skulski <jskulski@gmail.com>
2015-06-26 01:20:31 -0700

    Testing deploy on heroku

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Boneyard-Client
+
+React
+Flux
+Nanoajax
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var static = require('node-static');
 var file = new static.Server('./build');
 
-require('http').createServer(function (request, response) {
+var port = process.env.PORT || 5000;
 
+require('http').createServer(function (request, response) {
 
   request.addListener('end', function () {
     file.serve(request, response);
   }).resume();
 
-}).listen(8080);
+}).listen(port);
+
 console.log('Listening on http://localhost:8080/');
