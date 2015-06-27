938885f7697b089904f5a06a671056338f22ea3a
Jon Skulski <jskulski@gmail.com>
2015-06-26 17:39:19 -0700

    Adding nodemon to mix. Updating build scripts

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/build'));
 
-var urls_API_SHARD = '/api';
-app.use(urls_API_SHARD, _proxy_to_api);
+app.use('/api/', _proxy_to_api);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 function _proxy_to_api(req, res, next) {
+  console.log('hi');
   var host = 'http://boneyard-server.heroku.com/';
-  var api_url = url.resolve(host, req.path);
-  request(api_url).pipe(res);
+  //var api_url = url.resolve(host, req.path);
+  //request(api_url).pipe(res);
 }
 
